7f4fb68392f3a5df908fa5347e3c1e5bc898a855
Grove, Darren <darren.grove@blackstone.com>
2018-01-16 10:24:59 -0500

    move routing to before method calls

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var app = require('./app');
+var express = require('express');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: false }))
 app.set('port', (process.env.PORT || 3000));
-
-app.get('/', function (req, res) {
-		res.send('Hello Wrld!');
-});
-
-app.post('/darren', function(req, res) {
-		res.send('test post: ' + req.body.text);
-});
 
 var server = app.listen(app.get('port'), function () {
 		var host = server.address().address;
 		var port = server.address().port;
 
 		console.log('Example app listening at http://%s:%s', host, port);
+});
+
+app.get('/', function (req, res) {
+		res.send('Hello Wrld!');
+});
+
+app.post('/darren', function(req, res) {
+		res.send('tests post: ' + req.body.text);
 });
