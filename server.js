270270e9bb4ff6f61fc144de4c048d677871456d
Grove, Darren <darren.grove@blackstone.com>
2018-01-19 18:42:50 -0500

    enhance stock api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var app = require('./app');
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var json = require('JSON');
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,18 +21,28 @@ app.get('/', function (req, res) {
 
 app.post('/stock', function(req, res) {
 		var stockReq = req.body.stock;
-		var apiUrl = 'https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=' + stockReq + '&apikey=VVCZ3DAK6MZGR2XW';
+		var apiUrl = 'https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=' + stockReq + '&apikey=VVCZ3DAK6MZGR2XW'
 		
 		var options = {
 			uri: apiUrl
 		};
 		
 		if(req.headers.host.indexOf("localhost") > -1) {
-			options.proxy = "http://proxy.blackstone.com:8080";
+			options.proxy = "http://cs41cb06pxy03.blackstone.com:8080";
 		};
-
+		
 		request(options, function (error, response, body) {
-			res.send(body);
+			var stock = JSON.parse(body);
+			var resp = {};
+			resp.symbol = stock['Stock Quotes'][0]['1. symbol'];
+			resp.price = stock['Stock Quotes'][0]['2. price'];
+			
+			var text  = "\"attachments\": [ {\"fallback\" : \"Slack Default\""; 
+			text += ", \"color\": \"good\", \"fields\":[ { \"title\":\"" + resp.symbol + "\", \"value\":\"Current Price: " + resp.price + "%\" } ]"
+			text += "} ]";
+
+			res.setHeader("Content-type", "application/json");
+			res.send("{ \"response_type\": \"in_channel\"," + text + " }");
 		});
 });
 
