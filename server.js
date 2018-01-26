b70e3c5ae2a6da52c436ce9c999b10c738d669b7
Grove, Darren <darren.grove@blackstone.com>
2018-01-26 14:27:59 -0500

    update stock command to include percent change and such

--- server.js
+++ server.js
@@ -18,32 +18,64 @@ var server = app.listen(app.get('port'), function () {
 
 app.post('/stock', function(req, res) {
 		var stockReq = req.body.text;
-		var apiUrl = 'https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=' + stockReq + '&apikey=VVCZ3DAK6MZGR2XW'
+		var apiUrl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + stockReq + '&interval=15min&apikey=VVCZ3DAK6MZGR2XW'
 		
 		var options = {
 			uri: apiUrl
 		};
 		
 		if(req.headers.host.indexOf("localhost") > -1) {
-			options.proxy = "http://cs41cb06pxy01.blackstone.com:8080";
+			options.proxy = "http://cs41cb06pxy03.blackstone.com:8080";
 		};
 		
 		request(options, function (error, response, body) {
-			try {
+			 try {
 				var stock = JSON.parse(body);
+				var prices = stock['Time Series (15min)'];
+				var index = [];
+				
+				var today = new Date().getDate();
+
+				// build the index
+				for (var x in prices) {
+					var date = new Date(x).toLocaleString();
+					var today = new Date(Date.now()).toLocaleString();
+					if(date.substring(0,9) === today.substring(0, 9)) {
+						index.push(x);
+					};
+				};
+
+				// sort the index
+				index.sort(function (a, b) {    
+				   return a == b ? 0 : (a > b ? 1 : -1); 
+				}); 
+				
 				var resp = {};
-				resp.symbol = stock['Stock Quotes'][0]['1. symbol'];
-				resp.price = stock['Stock Quotes'][0]['2. price'];
+				resp.time = index[index.length-1];
+				var latest = prices[resp.time];
+				var earliest = prices[index[0]];
+				var open = earliest['1. open'];
+				var close = latest['4. close'];
+				resp.symbol = stock['Meta Data']['2. Symbol'].toUpperCase();
+				resp.current = close;
+				resp.change = (close - open).toFixed(2);
+				resp.changePercent = (((close - open) / open)*100).toFixed(2);
+				resp.time = index[index.length-1];
 				
 				var text  = "\"attachments\": [ {\"fallback\" : \"Slack Default\""; 
-				text += ", \"color\": \"#439FE0\", \"fields\":[ { \"title\":\"" + resp.symbol + "\", \"value\":\"Current Price: " + resp.price + "\" } ]"
+				if(stock.ChangePercent < 0) {
+					text += ", \"color\": \"danger\", \"fields\":[ { \"title\":\"" + resp.symbol + " - Last Updated: " + resp.time + "\", \"value\":\"Last Price: " + resp.current + ",  " + resp.change + "    " + resp.changePercent + "%\" } ]"
+				} else {
+					text += ", \"color\": \"good\", \"fields\":[ { \"title\":\"" + resp.symbol + " - Last Updated: " + resp.time + "\", \"value\":\"Last Price " + resp.current + ",  " + resp.change + "    " + resp.changePercent + "%\" } ]"
+				}
+				
 				text += "} ]";
 
 				res.setHeader("Content-type", "application/json");
 				res.send("{ \"response_type\": \"in_channel\"," + text + " }");
-			} catch (err) {
-				res.send("Incorrect input, please try again");
-			}
+			 } catch (err) {
+				 res.send("Incorrect input, please try again");
+			 }
 		});
 });
 
