ffd4d8446a8b1551956b92ff7f70e771a28b64f1
skijmb <jeffreymberk@gmail.com>
2018-09-28 14:39:10 -0400

    Personalized error messaging

--- stock.js
+++ stock.js
@@ -141,7 +141,19 @@ var formatStockResults = function(apiResp, filter, requestData) {
 	} else {
 		attachment.color = "#000000";
 		fields.title = stockInfo.symbol + " is not a valid symbol";
-		fields.value = requestData.user_name + " pls";
+		var nickname = "";
+		if (requestData.user_name = "irabulog") {
+			nickname = "ira"
+		} else if (requestData.user_name = "bsnaxx") {
+			nickname = "brn"
+		} else if (requestData.user_name = "dgrove153") {
+			nickname = "drn"
+		} else if (requestData.user_name = "j3ff") {
+			nickname = "jef"
+		} else {
+			nickname = requestData.user_name;
+		}
+		fields.value = nickname + " pls";
 	};
 	
 	attachment.fields = [fields];
